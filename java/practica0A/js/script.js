f8ec4b81869ed7bc698038f66cbd4a437f41f771
Patoo27 <sofia.delgado2027@frech.superate.org>
2026-08-17 10:45:20 -0600

    ...

---
+++ java/practica0A/js/script.js
@@ -0,0 +1,8 @@
+function revisar(numero){
+    let caja = document.getElementById ("mensaje")
+    //obtener el parrafo donde ira el mensaje
+    caja.innerHTML = "Respuesta correcta! todo gucci"
+    caja.classList.remove("incorrecto")
+    caja.classList.add("correcto")
+}
+
---
+++ java/practica0js/JS/script.js
@@ -0,0 +1,6 @@
+alert ("Hola, soy una alerta creada por Sofi");
+
+document.write ("Hola, soy un document.write");
+
+console.log ("soy un mensaje en la consola");
+
