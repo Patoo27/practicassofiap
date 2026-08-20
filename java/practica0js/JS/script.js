f6cd213b400be1bc8a44e6016c4aac7cf4e34027
Patoo27 <sofia.delgado2027@frech.superate.org>
2026-08-20 08:27:42 -0600

    aa

--- java/practica0js/JS/script.js
+++ java/practica0js/JS/script.js
@@ -4,3 +4,16 @@ document.write ("Hola, soy un document.write");
 
 console.log ("soy un mensaje en la consola");
 
+//tipos de datos//
+let nombre= "ana";
+let edad = 18;
+let estudiante = true;
+let ciudad;
+let mascota =null;
+
+//usando typeof para identificar cada tipo//
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof estudiante);
+console.log(typeof ciudad);
+console.log(typeof mascota);
